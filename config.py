1f3da50e245318d416b6cd41b85ac88e1da48351
RAJ RAUTRAY <153398574+ASIFXQUEEN@users.noreply.github.com>
2025-04-29 00:05:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,13 @@
 import os
-API_ID = int(os.environ.get("API_ID", 12345))
-API_HASH = os.environ.get("API_HASH", "")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+
+class Config:
+    # Telegram (Required)
+    API_ID = int(os.environ.get("API_ID", 12345))  # my.telegram.org se
+    API_HASH = os.environ.get("API_HASH", "")      # my.telegram.org se
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")    # @BotFather se
+    
+    # Owner
+    OWNER_ID = int(os.environ.get("OWNER_ID", 123456789))  # @userinfobot se apna ID lein
+    
+    # Proxy (Optional)
+    PROXY = None  # {"host":"1.1.1.1", "port":80, "scheme":"socks5"}
